3d60c12f11655c4bfebfab87cc9b38c31fa7416f
Alice Bevan-McGregor <alice.mcgregor@me.com>
2009-08-26 00:28:09 -0700

    Continuing restore from dead laptop; actual code is now present.

--- cos/__init__.py
+++ cos/__init__.py
@@ -1,13 +1,15 @@
 
 
-#
+# Standard priority values.
 PRIORITY_CRITICAL   =  1.0
 PRIORITY_HIGH       =  0.5
 PRIORITY_NORMAL     =  0.0
 PRIORITY_LOW        = -0.5
 PRIORITY_IDLE       = -1.0
 
-Hz = 1
+
+# Scheduling frequency constants.
+Hz = 1.0
 KHz = 1000 * Hz
 MHz = 1000 * KHz
 GHz = 1000 * MHz
