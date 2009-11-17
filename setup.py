55d1635feedd3f2809b51635390d598e14db1cb0
Alice Bevan-McGregor <alice.mcgregor@me.com>
2009-11-16 19:09:55 -0800

    Released and preped for next update.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
           name = "CoScheduler",
-          version = "0.1",
+          version = "0.2",
           description = "A coroutine-based task scheduler and simulation framework.",
           author = "Alice Bevan-McGregor",
           author_email = "alice@gothcandy.com",
